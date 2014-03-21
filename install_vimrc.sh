4232f4f615bf1bbcad9c6bee061b37fad374ae75
Miguel Martin <miguel@miguel-martin.com>
2014-03-21 13:39:52 +1030

    Added a note in install_vimrc.sh to user

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -4,3 +4,6 @@ echo 'copying ./vimrc to ~/.vimrc'
 
 # copy the vim file
 cp ./vimrc ~/.vimrc
+
+echo 'succesfully copied ./vimrc to ~/.vimrc'
+echo 'note: you require to :BundleInstall once you first open VIM'
