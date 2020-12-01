a5bd8bfba4cae2e3db6b5173a6c502e16560c1ba
Miguel Martin <miguel@miguel-martin.com>
2020-11-30 16:04:57 -0800

    add emacs config

---
+++ tilde/.emacs.d/init.el
@@ -0,0 +1,51 @@
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Install Minimal Packages
+;; =============================
+
+;; org-mode
+;; ----------
+(use-package org :ensure t)
+
+;; evil mode
+;; ---------
+(use-package undo-tree :init (global-undo-tree-mode))
+(use-package evil
+  :after undo-tree
+  :ensure t
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal)
+
+  (setq evil-undo-system 'undo-tree)
+)
+
+;; Load org config
+(org-babel-load-file "~/.emacs.d/myinit.org")
