ee59560468c35a310a40f3c5d885d476e6f66a9a
Miguel Martin <miguelmartin@fb.com>
2020-12-22 23:32:47 -0800

    doom config iteration

--- tilde/.doom.d/init.el
+++ tilde/.doom.d/init.el
@@ -149,7 +149,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam +evil-org +pretty +present +journal)
+       (org +roam +evil-org +pretty +present)
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
@@ -177,7 +177,7 @@
        ;;(wanderlust +gmail)
 
        :app
-       ;;calendar
+       calendar
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
